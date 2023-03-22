599c88a160be46e47364a54a4e7b96162eafc404
MeetButani27 <20cs010@charusat.edu.in>
2023-03-22 22:58:58 +0530

    Fixed add item form

--- src/components/AddItem.js
+++ src/components/AddItem.js
@@ -6,7 +6,7 @@ import SendIcon from '@mui/icons-material/Send';
 import TextField from '@mui/material/TextField';
 
 export default function AddItem() {
-  const [new_item, setNew_item] = useState({
+  const [items, setItems] = useState({
     item_id: "",
     item_name: "",
     price: "",
@@ -15,46 +15,73 @@ export default function AddItem() {
     item_url: ""
   });
 
-  const handleSubmit = () => {
-    console.log('Product added successfully!!');
+  let name, value;
+
+  const handleChange = (e) => {
+    name = e.target.name;
+    value = e.target.value;
+    setItems({...items, [name]:value});
+  };
+
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
   };
 
   return (
     <div className="my-5">
-      <Box component="form" noValidate autoComplete="off" style={{'display':"flex",'justifyContent':"center", 'alignItems':"center"}}
+      <Box component="" noValidate autoComplete="off" style={{'display':"flex",'justifyContent':"center", 'alignItems':"center"}}
         sx={{
           '& .MuiTextField-root': { m: 2, width: '45ch', display: 'flex' },
         }}
       >
-        <div style={{'textAlign': 'center'}}>
+        <form style={{'textAlign': 'center'}}>
           <h3 className="mb-4">Add a new product</h3>
           <TextField
             id="outlined-required"
+            name="item_id"
             label="Item ID"
+            value={items.item_id}
+            onChange={(e) => handleChange(e) }
           />
           <TextField
             id="outlined-required"
+            name="item_name"
             label="Item name"
+            value={items.item_name}
+            onChange={(e) => handleChange(e) }
           />
           <TextField
             id="outlined-required"
+            name="price"
             label="Price"
+            value={items.price}
+            onChange={(e) => handleChange(e) }
           />
           <TextField
             id="outlined-required"
+            name="rating"
             label="Rating"
+            value={items.rating}
+            onChange={(e) => handleChange(e) }
           />
           <TextField
             id="outlined-required"
+            name="category"
             label="Category"
+            value={items.category}
+            onChange={(e) => handleChange(e) }
           />
           <TextField
             id="outlined-required"
+            name="item_url"
             label="Item URL"
+            value={items.item_url}
+            onChange={(e) => handleChange(e) }
           />
 
-          <Button variant="contained" className="my-4 px-5" onClick={handleSubmit()} endIcon={<SendIcon />}> Add product </Button>
-        </div>
+          <Button variant="contained" className="my-4 px-5" type="submit" onChange={(e) => handleSubmit(e)} endIcon={<SendIcon />}> Add product </Button>
+        </form>
       </Box>
     </div>
   );
